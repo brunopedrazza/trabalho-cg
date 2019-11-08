6be833c5a23159e07f7930622f307ff487b35fcd
Bruno Pedrazza <pedrazzabruno@gmail.com>
2019-11-08 20:06:18 -0300

    varios ajustes

--- src/js/upload.js
+++ src/js/upload.js
@@ -5,5 +5,7 @@ onload = function(){
 var loadFile = function(event) {
     var output = document.getElementById('output');
     output.src = URL.createObjectURL(event.target.files[0]);
+    output.style.border = "dotted";
+    output.style.borderWidth = "3px";
     this.document.getElementById("submit").style.display = "inline";
 };
