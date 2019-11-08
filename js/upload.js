42f83d12368a6efb489e2e89e72335bf6a7e734d
Bruno Pedrazza <Bruno@Brunos-MacBook-Pro.local>
2019-11-07 22:57:01 -0300

    botao de upload mais bonito

--- js/upload.js
+++ js/upload.js
@@ -1,4 +1,25 @@
+onload = function(){
+    this.document.getElementById("submit").addEventListener("click", submitImage);
+}
+
 var loadFile = function(event) {
     var output = document.getElementById('output');
     output.src = URL.createObjectURL(event.target.files[0]);
+    var button = this.document.getElementById("button");
+    if (!button.firstChild) {
+        createSendButton(button);
+    }
 };
+
+function submitImage(){
+
+}
+
+function createSendButton(button){
+    var objButtonSubmit = document.createElement("input");
+    var text = document.createTextNode("Enviar");
+    objButtonSubmit.setAttribute("type", "submit");
+    objButtonSubmit.setAttribute("form", "form");
+    objButtonSubmit.appendChild(text);
+    button.appendChild(objButtonSubmit);
+}
