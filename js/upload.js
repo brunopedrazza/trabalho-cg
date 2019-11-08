cc99881035fbdc35d7c892e119713ce0909af755
Bruno Pedrazza <pedrazzabruno@gmail.com>
2019-11-08 00:04:16 -0300

    ajustando botoes

--- js/upload.js
+++ js/upload.js
@@ -1,25 +1,9 @@
 onload = function(){
-    this.document.getElementById("submit").addEventListener("click", submitImage);
+    this.document.getElementById("submit").style.display = "none";
 }
 
 var loadFile = function(event) {
     var output = document.getElementById('output');
     output.src = URL.createObjectURL(event.target.files[0]);
-    var button = this.document.getElementById("button");
-    if (!button.firstChild) {
-        createSendButton(button);
-    }
+    this.document.getElementById("submit").style.display = "inline";
 };
-
-function submitImage(){
-
-}
-
-function createSendButton(button){
-    var objButtonSubmit = document.createElement("input");
-    var text = document.createTextNode("Enviar");
-    objButtonSubmit.setAttribute("type", "submit");
-    objButtonSubmit.setAttribute("form", "form");
-    objButtonSubmit.appendChild(text);
-    button.appendChild(objButtonSubmit);
-}
